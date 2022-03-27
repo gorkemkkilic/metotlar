b253c9f9f0123b4a1ed5725ba55e27b8f5cc6c84
Görkem <Grkem@DESKTOP-3VGVQ6F>
2022-03-27 14:09:57 +0300

    Proje dosyası ekle.

---
+++ matematık/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace matematık
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DörtIslem dortIslem = new DörtIslem();
+            dortIslem.Topla(5, 6);        }
+    }
+}
